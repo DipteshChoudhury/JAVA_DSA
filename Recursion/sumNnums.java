d098beea40996f7a36abb18f935ecc7743dd22a4
DipteshChoudhury <mrdiptesh1104@gmail.com>
2023-04-15 17:42:36 +0530

    sum of n nums

---
+++ Recursion/sumNnums.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class sumNnums {
+    public static long sumN(long num) {
+        if (num == 0) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
+        }
+
+        return num + sumN(num - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a num(to find sum of n nums) : ");
+        long n = sc.nextLong();
+
+        if (n < 0) {
+            System.out.println("Invalid Num!!");
+            return;
+        }
+        System.out.println("Sum of " + n + " nums : " + sumN(n));
+        sc.close();
+    }
+}
