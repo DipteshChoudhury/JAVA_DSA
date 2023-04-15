1cde0f29531fd209940d1291953a0fd2ff10fbbf
DipteshChoudhury <mrdiptesh1104@gmail.com>
2023-04-15 18:04:56 +0530

    Nth fibonacci num

---
+++ Recursion/NthFibonacciNum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class NthFibonacciNum {
+    public static int nFibo(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 1;
+        }
+
+        return nFibo(n - 1) + nFibo(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter +ve number :- ");
+        int n = sc.nextInt();
+
+        System.out.println(n + "th fibonacci num :- " + nFibo(n));
+        sc.close();
+    }
+}
